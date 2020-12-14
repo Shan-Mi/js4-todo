2d248669c7c5a56c479e7bb62b8388ef49288aa9
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-12-14 09:47:49 +0100

    Hard coded data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from "react";
 
+const todoList = [
+  {
+    id: 1,
+    task: "Do the Laundry!",
+    dueDate: new Date(),
+    isCompleted: false,
+    priority: 3,
+  },
+  {
+    id: 2,
+    task: "Do the Dishes!",
+    dueDate: new Date(),
+    isCompleted: false,
+    priority: 2,
+  },
+  {
+    id: 3,
+    task: "Buy milk!",
+    dueDate: new Date(),
+    isCompleted: false,
+    priority: 1,
+  },
+  {
+    id: 4,
+    task: "Drop garbage!",
+    dueDate: new Date(),
+    isCompleted: false,
+    priority: 4,
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <h1>Todo, believe or not</h1>
+      {todoList.map((todoItem) => (
+        <p>{todoItem.task}</p>
+      ))}
     </div>
   );
 }
